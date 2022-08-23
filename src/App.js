b38278104f72900213c2434eefbbca7099a6f05a
Alex Lindemann <AlexDeadmanLindemann@gmail.com>
2022-08-23 13:39:13 -0500

    Testing

--- src/App.js
+++ src/App.js
@@ -77,10 +77,6 @@ function App() {
         <button onClick={() => {
           navigator.share({ text: "test", title: "Title", url: "http://web.dev" });
         }}>Share</button>
-        <a href={`intent://test-installable-two.herokuapp.com${hashTarget ? "#" + hashTarget : ''};scheme=https;package=org.chromium.webapk.ac3efa232bdbc71ef_v2;end`}>test intent</a>
-        <a href={`intent://test-installable-two.herokuapp.com${hashTarget ? "#" + hashTarget : ''};scheme=https;action=android.intent.action.VIEW;end;`}>test intent 2</a>
-        <a href={`intent://test-installable-two.herokuapp.com${hashTarget ? "#" + hashTarget : ''}#Intent;scheme=https;package=org.chromium.webapk.ac3efa232bdbc71ef_v2;end`}>test intent</a>
-        <a href={`intent://test-installable-two.herokuapp.com${hashTarget ? "#" + hashTarget : ''}#Intent;scheme=https;action=android.intent.action.VIEW;end;`}>test intent 2</a>
       </header>
     </div>
   );
